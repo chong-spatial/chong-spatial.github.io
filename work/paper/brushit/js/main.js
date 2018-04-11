1f480b1dee89dacc3c81019d9d6e1078603db47c
chongzhang-vis <chongzhang.nc@gmail.com>
2018-04-11 00:38:02 -0700

    change color

--- work/paper/brushit/js/main.js
+++ work/paper/brushit/js/main.js
@@ -5,8 +5,10 @@ var pointsInServiceAreas = {'hotel':{}, 'attraction':{}};
 
 var colorbar4time = ['#bd0026', '#feb24c', '#d9ef8b', '#c7e9b4', '#7bccc4'];
 //var colorbar4timeRGB = [[189,0,30,0.6], [254,178,76,0.25], [217,239,139,0.25], [199,233,180,0.25], [123,204,196,0.6]];
-var colorbar4timeRGB = [[], [71,255,4,0.25], [64, 171, 171,0.25], [140,177,241,0.25], []];
-var colorStroke4timeRGB = [[], [255,255,0], [226,133,151], [219,125,226], []];
+// var colorbar4timeRGB = [[], [71,255,4,0.25], [64, 171, 171,0.25], [140,177,241,0.25], []];
+var colorbar4timeRGB = [[], [71,255,4,0.25], [255, 255, 0,0.25], [255, 193, 7,0.25], []];
+// var colorStroke4timeRGB = [[], [255,255,0], [226,133,151], [219,125,226], []];
+var colorStroke4timeRGB = [[], [127, 240, 127], [235, 217, 63], [230, 83, 83], []];
 
 /*
 d3.select("#colorSpans").selectAll('span')
