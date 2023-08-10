7a4cc8d7a68a32292fbf732e6e982544f81b8ad8
Chong <chongzhang.nc@gmail.com>
2023-08-10 12:26:46 +0800

    three.js+mapbox;fix poi;

---
+++ work/threemapbox/config.js
@@ -0,0 +1,4 @@
+var config = {
+    accessToken: 'pk.eyJ1IjoiZmlzaGluZ2xha2UiLCJhIjoiY2xsNGhneTlzMDd2bDNja2Q5ZWFmNzF3bCJ9._xyYBnBEy9QqY9Sq-jUjdA',
+    subscriptionKey: 'azure maps subscription key goes here'
+};
